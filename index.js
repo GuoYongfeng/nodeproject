88fec22367d9fc546172fc3053e2b0d250a4ac02
guoyongfeng <296512521@qq.com>
2015-01-21 23:14:24 +0800

    save

--- index.js
+++ index.js
@@ -1,7 +1,36 @@
-function hello(){
-	console.log('hello world');
-}
+var express = require('express');
+var port = process.env.PORT || 3000;
 
-hello();
+var app = express();
 
+app.set('view engine', 'jade');
+app.set('views', './views');
+
+app.listen(port);
+
+console.log('app started on port: ' + port);
+
+app.get('/', function(req, res){
+	res.render('index', {
+		title: '首页',
+	});
+});
+
+app.get('/movie/:id', function(req, res){
+	res.render('detail', {
+		title: '详情页',
+	});
+});
+
+app.get('/admin/movie', function(req, res){
+	res.render('admin', {
+		title: '后台录入页',
+	});
+});
+
+app.get('/admin/list', function(req, res){
+	res.render('list', {
+		title: '列表页',
+	});
+});
 
