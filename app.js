42e8aeecdf3d7330d147a3cf27efa2018a8252ca
guoyongfeng <296512521@qq.com>
2015-01-22 21:17:18 +0800

    add modules

--- app.js
+++ app.js
@@ -3,7 +3,40 @@
  * Module dependencies.
  */
 
-var express = require('fs');
+var express = require('express');
+var path = require('path');
+var port = process.env.PORT || 3000;
 var app = express();
 
-app.set('view engine', jade);
+app.set('views', './views/pages');
+app.set('view engine', 'jade');
+app.use(express.bodyParser());
+app.use(express.static(path.join(__dirname, 'bower_components')));
+
+app.listen(port);
+
+console.log('server started on port: ' + port );
+
+app.get('/', function(req, res){
+	res.render('index', {
+		title: '首页'
+	});
+});
+
+app.get('/admin/:id', function(req, res){
+	res.render('detail', {
+		title: '详情页'
+	});
+});
+
+app.get('/admin/list', function(req, res){
+	res.render('list', {
+		title: '列表页'
+	});
+});
+
+app.get('/admin/movie', function(req, res){
+	res.render('admin', {
+		title: '后台录入页'
+	});
+});
